843c82b282a00181149ee0ae85a4e4fdee3b6e7d
Nathalia Tamada <133176496+Naritmd@users.noreply.github.com>
2024-09-10 21:47:03 -0300

    Add files via upload

--- 06threads_02/src/controller/ThreadAeroporto.java
+++ 06threads_02/src/controller/ThreadAeroporto.java
@@ -1,5 +1,108 @@
 package controller;
-
-public class ThreadAeroporto {
-
+import java.util.concurrent.Semaphore;
+public class ThreadAeroporto extends Thread
+{
+	private Semaphore semaforo;
+	private int aviao;
+	
+	public ThreadAeroporto (Semaphore semaforo, int aviao)
+	{
+		this.semaforo = semaforo;
+		this.aviao = aviao;
+		
+	}
+	
+	@Override
+	
+	public void run()
+	{
+		manobrar();
+		try 
+		{
+			semaforo.acquire();
+			taxiar();
+			decolar();
+		}
+		catch (InterruptedException e)
+		{
+			System.err.println(e.getMessage());
+		}
+		finally
+		{
+			semaforo.release();
+			afastar();
+		}
+		
+	}
+	
+	private void manobrar()
+	{ 
+		int tempo = (int) ((Math.random() * 701) + 300);
+		try 
+		{
+			sleep(tempo);
+			System.out.println("Avião # " +aviao+ "manobrou em " +tempo+ " ms.");
+		}
+		catch (InterruptedException e) 
+		{
+		System.err.println(e.getMessage());	
+		}
+	}
+	
+	private void taxiar ()
+	{
+		int tempo = (int) ((Math.random() * 1001 ) +500);
+		try 
+		{
+			sleep(tempo);
+			if (tempo % 2 == 0)
+			{
+				System.out.println("Avião #" + aviao+ " taxiou em " +tempo+ "ms.");
+			}
+			else
+			{
+				System.out.println("Avião #" +aviao+ " taxiou em " +tempo+ "ms.");
+			}
+		}
+		catch (InterruptedException e)
+		{
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	private void decolar()
+	{
+		int tempo = (int) ((Math.random() * 801) + 600);
+		try 
+		{
+			sleep(tempo);
+			if (tempo % 2 == 0)
+			{
+				System.out.println("Avião #" +aviao + "decolou na pista norte.");
+			}
+			else
+			{
+				System.out.println("Avião #" +aviao+ "decolou na pista norte");
+			}
+		}
+		catch (InterruptedException e)
+		{
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	private void afastar()
+	{
+		int tempo = (int) ((Math.random() * 801) + 300);
+		
+		try
+		{
+			sleep(tempo);
+			System.out.println("Avião # " +aviao+ " se afastou em " +tempo+ "ms.");
+		}
+		catch (InterruptedException e)
+		{
+			System.err.println(e.getMessage());
+		}
+	}
 }
--- 06threads_02/src/view/ThreadAeroportoPrincipal.java
+++ 06threads_02/src/view/ThreadAeroportoPrincipal.java
@@ -1,5 +1,26 @@
+//2. Um aeroporto tem 2 pistas (norte e sul) e, em cada pista, apenas um avião pode fazer a
+//decolagem. O procedimento de decolagem tem 4 fases ( taxiar, decolagem e afastamento da área).
+//A fase de manobra pode durar de 300 a 700 milissegundos A fase de taxiar, de 500 a 1000
+//milissegundos. A fase de decolagem, de 600 a 800 milissegundos. A fase de afastamento, de 300 a
+//800 milissegundos. O aeroporto reúne, por ciclo, 12 aeronaves que podem decolar pela pista norte
+//ou pela pista sul (decisão aleatória) mas, apenas 2 aviões podem circular pela área de decolagem
+//ao mesmo tempo. Fazer uma aplicação em Java que resolva o problema.
+
 package view;
 
-public class ThreadAeroportoPrincipal {
+import java.util.concurrent.Semaphore;
+import controller.ThreadAeroporto;
 
+public class ThreadAeroportoPrincipal 
+{
+	public static void main (String args[])
+	{
+		Semaphore semaforo = new Semaphore(2);
+		
+		for (int i = 0; i < 3; i++)
+		{
+			Thread t = new ThreadAeroporto(semaforo, i);
+			t.start();
+		}
+	}
 }
